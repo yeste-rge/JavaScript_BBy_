885b31a6f23097c70405b486fb05c364e853b5ea
yeste-rge <Cheongziqian20@gmail.com>
2024-02-06 13:29:50 +0800

    add pluplus.js and edit code.js and assignment.js

--- code.js
+++ code.js
@@ -1,10 +1,27 @@
+//3.0_变数与运算算子
 let a=1;
 console.log(a);
 
 let characterHP = 100;
 console.log(characterHP);
 
+let myHp = 200;
+console.log(myHp);
+
 let noTnitialValue;
 
-let sum = 1 + 2 + 3;
-console.log(sum);
+myHp = 1 + 200 + 3;
+console.log(myHp);
+//3.1_懒人运算子 += -= *= /= // ++ -- ** //
+// myHp = myHp - 3;
+myHp -= 3;
+console.log(myHp);
+
+myHp += 5;
+console.log(myHp);
+
+myHp *= 2;
+console.log(myHp);
+
+myHp /= 3;
+console.log(myHp)
---
+++ plusplus.js
@@ -0,0 +1,12 @@
+//3.1 懒人运算子
+let i = 0;
+console.log(i);
+
+i = i + 1;
+console.log(i);
+
+i += 1;
+console.log(i);
+
+i++;
+console.log(i);
