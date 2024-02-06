5c05a429b4a5e4cfc1ed32eb52589d18f4716e6a
yeste-rge <Cheongziqian20@gmail.com>
2024-02-06 14:36:56 +0800

    add test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+//3.7 变数的型别:数学与字串
+console.log("Hello World");
+console.log(1.234e3);
+console.log('I\'m james');
+console.log("I'm"+" James");
+console.log("123"+"456");
+console.log("123"-"1"); //number
+const str ='123'
+console.log(str[0]) //1
+console.log(str[1]) //2
+// console.log(str[5]) //undefined
+console.log(str.length) //3 number
+// console.log(str.length*2) //6 number
+// console.log(str.length - 1) //2 number
+// console.log(typeof 1) //number
+// console.log(typeof '123123')//string
+// const stra = 'jaksfdl;lksad';
+// console.log(stra) //jaksfdl;lksad
+// console.log(typeof str) // string
