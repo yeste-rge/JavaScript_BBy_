63a64f4b43290b27d63f0bd286ebe7de5a7d50ed
yeste-rge <Cheongziqian20@gmail.com>
2024-02-13 12:27:48 +0800

    add 15code.js

---
+++ 15code.js
@@ -0,0 +1,18 @@
+//4.3_i+ vs. ++i
+let i = 0;
+console.log(i++); //0
+//（后置递增）(Post-increment)
+console.log(i); //1
+console.log("---------"); //---------
+i = 0;
+console.log(++i); //1
+//（前置递增）(Pre-increment)
+console.log(i); //1
+
+//extra 4.3_i-- vs. --i
+let x = 1;
+console.log(i--); //1
+console.log(i); //0
+let g = 1;
+console.log(--g); //0
+console.log(g); //0
