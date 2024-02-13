9cba31410c767b3857f7f845c7236aae0cfefcbd
yeste-rge <Cheongziqian20@gmail.com>
2024-02-13 12:26:56 +0800

    3code.js del 4.3 and extra 4.3

--- 3code.js
+++ 3code.js
@@ -25,22 +25,3 @@ console.log(myHp);
 
 myHp /= 3;
 console.log(myHp);
-
-//4.3_i+ vs. ++i
-let i = 0;
-console.log(i++); //0
-//（后置递增）(Post-increment)
-console.log(i); //1
-console.log("---------"); //---------
-i = 0;
-console.log(++i); //1
-//（前置递增）(Pre-increment)
-console.log(i); //1
-
-//extra 4.3_i-- vs. --i
-let x = 1;
-console.log(i--); //1
-console.log(i); //0
-let g = 1;
-console.log(--g); //0
-console.log(g); //0
